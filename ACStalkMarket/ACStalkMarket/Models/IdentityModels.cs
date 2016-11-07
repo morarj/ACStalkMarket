3f6000ff71c1a1536b5b68397996cde1818ea2af
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-11-06 22:25:09 -0600

    Add People and Gender models

--- ACStalkMarket/ACStalkMarket/Models/IdentityModels.cs
+++ ACStalkMarket/ACStalkMarket/Models/IdentityModels.cs
@@ -3,12 +3,17 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace ACStalkMarket.Models
 {
     // Puede agregar datos del perfil del usuario agregando más propiedades a la clase ApplicationUser. Para más información, visite http://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
+        public People People { get; set; }
+        [Required]
+        public int PeopleId { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Tenga en cuenta que el valor de authenticationType debe coincidir con el definido en CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +25,9 @@ namespace ACStalkMarket.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<People> People { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ ACStalkMarket/ACStalkMarket/Models/People.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ACStalkMarket.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Nombre")]
+        [RegularExpression("")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime? BirthDate { get; set; }
+
+        public Gender Gender { get; set; }
+        [Required]
+        [Display(Name = "Género")]
+        public short GenderId { get; set; }
+    }
+}
