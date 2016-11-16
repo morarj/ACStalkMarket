a086cce087783dd2e598476c884aa7540f2275a2
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-11-15 21:51:52 -0600

    Add Week classes

---
+++ ACStalkMarket/ACStalkMarket/Models/Week.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ACStalkMarket.Models
+{
+    public class Week
+    {
+        public int Id { get; set; }
+
+        public People People { get; set; }
+        [Required]
+        public int PeopleId { get; set; }
+
+        public Town Town { get; set; }
+        [Required]
+        public int TownId { get; set; }
+
+        public WeekValues WeekValues { get; set; }
+        [Required]
+        public int WeekValuesId { get; set; }
+
+        public WeekPattern WeekPattern { get; set; }
+        [Required]
+        public byte WeekPatternId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        // Must be monday
+        public DateTime StartingDate { get; set; }
+
+        [Required]
+        [Range(1, byte.MaxValue)]
+        public byte TurnipStartingPrice { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int BellsInvestment { get; set; }
+
+        [Required]
+        public bool WeekActive { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Profit { get; set; }
+    }
+}
---
+++ ACStalkMarket/ACStalkMarket/Models/WeekValues.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ACStalkMarket.Models
+{
+    public class WeekValues
+    {
+        public int Id { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Lunes AM")]
+        public short monAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Lunes PM")]
+        public short monPM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Martes AM")]
+        public short tueAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Martes AM")]
+        public short tuePM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Miércoles AM")]
+        public short wedAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Miércoles AM")]
+        public short wedPM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Jueves AM")]
+        public short thuAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Jueves AM")]
+        public short thuPM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Viernes AM")]
+        public short friAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Viernes AM")]
+        public short friPM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Sábado AM")]
+        public short satAM { get; set; }
+
+        [Range(1, 999)]
+        [Display(Name = "Sábado AM")]
+        public short satPM { get; set; }
+    }
+}
