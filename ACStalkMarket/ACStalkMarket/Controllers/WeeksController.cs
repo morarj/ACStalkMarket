788e24a5f26c2240e586e897a3b26c7555e32600
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-11-16 02:24:56 -0600

    Add Weeks Controller with New and Save actions

---
+++ ACStalkMarket/ACStalkMarket/Controllers/WeeksController.cs
@@ -0,0 +1,115 @@
+﻿using ACStalkMarket.Models;
+using ACStalkMarket.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ACStalkMarket.Extensions;
+
+namespace ACStalkMarket.Controllers
+{
+    public class WeeksController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public WeeksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Weeks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Weeks/New
+        public ActionResult New()
+        {
+            var personId = Convert.ToInt32(User.Identity.GetPeopleId());
+            var town = _context.Towns.Single(p => p.PeopleId == personId);
+
+            var viewModel = new WeekValuesFormViewModel();
+            viewModel.WeekValues = new WeekValues();
+            viewModel.Week = new Week()
+            {
+                PeopleId = personId,
+                StartingDate = DateTime.Today,
+                TownId = town.Id,
+                WeekActive = true,
+                WeekPatternId = 1
+            };
+
+            return View("WeekValuesForm", viewModel);
+        }
+
+        // GET: Weeks/Edit
+        public ActionResult Edit(int id)
+        {
+            var weekInDB = _context.Weeks.SingleOrDefault(w => w.Id == id);
+            var weekValuesInDB = _context.WeekValues.SingleOrDefault(w => w.Id == weekInDB.WeekValuesId);
+
+            if (weekInDB == null || weekValuesInDB == null)
+                return HttpNotFound();
+
+            var viewModel = new WeekValuesFormViewModel()
+            {
+                Week = weekInDB,
+                WeekValues = weekValuesInDB
+            };
+
+            return View("WeekValuesForm", viewModel);
+        }
+
+        // POST: Weeks/Save
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(WeekValuesFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("WeekValuesForm", viewModel);
+            }
+
+            if (viewModel.WeekValues.Id == 0)
+            {
+                _context.WeekValues.Add(viewModel.WeekValues);
+                try
+                {
+                    _context.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                var week = new Week();
+                week = viewModel.Week;
+                week.WeekValuesId = viewModel.WeekValues.Id;
+
+                _context.Weeks.Add(week);
+            }
+            else
+            {
+                var weekValuesInDB = _context.WeekValues.Single(w => w.Id == viewModel.WeekValues.Id);
+                var weekInDB = _context.Weeks.Single(w => w.Id == viewModel.Week.Id);
+
+                weekValuesInDB = viewModel.WeekValues;
+                weekInDB = viewModel.Week;
+            }
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+
+            }
+
+            return RedirectToAction("Index", "Weeks");
+        }
+    }
+}
