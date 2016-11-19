b9da0c13c06b59174c0ee217bb57ae6ceee99092
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-11-18 21:52:39 -0600

    Start AutoMapper configuration

---
+++ ACStalkMarket/ACStalkMarket/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACStalkMarket.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domains to DTOs
+            // DTOs to Domains
+        }
+    }
+}
