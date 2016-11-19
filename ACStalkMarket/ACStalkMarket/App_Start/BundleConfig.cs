02825e0c47098cfc09cc605bbcb07d02804a6552
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-11-19 00:40:48 -0600

    Group & Add DataTables Scripts to BundleConfig

--- ACStalkMarket/ACStalkMarket/App_Start/BundleConfig.cs
+++ ACStalkMarket/ACStalkMarket/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace ACStalkMarket
         // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,11 +23,8 @@ namespace ACStalkMarket
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
